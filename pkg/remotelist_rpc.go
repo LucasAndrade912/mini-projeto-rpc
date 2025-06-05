c8059dfdca2e3b8ea19081e3a0a2e8f5fe8ac784
Lucas Andrade <lucasantos.dev@gmail.com>
2025-06-04 22:30:45 -0300

    feat: implementacao de logica para criar snapshots

--- pkg/remotelist_rpc.go
+++ pkg/remotelist_rpc.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 type List struct {
@@ -171,5 +172,72 @@ func NewRemoteList() *RemoteList {
 		fmt.Println("No previous log file found. Starting with empty lists.")
 	}
 
+	go startSnapshotWorker(remoteList)
+
 	return remoteList
+}
+
+func startSnapshotWorker(remoteList *RemoteList) {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	fmt.Println("Snapshot worker started. Snapshots will be created every minute.")
+
+	for range ticker.C {
+		createSnapshot(remoteList)
+	}
+}
+
+func createSnapshot(remoteList *RemoteList) {
+	for i := 0; i < remoteList.Count; i++ {
+		remoteList.Lists[i].mu.Lock()
+	}
+
+	defer func() {
+		for i := 0; i < remoteList.Count; i++ {
+			remoteList.Lists[i].mu.Unlock()
+		}
+	}()
+
+	snapshotsDir := "snapshots"
+	if _, err := os.Stat(snapshotsDir); os.IsNotExist(err) {
+		err := os.Mkdir(snapshotsDir, 0755)
+		if err != nil {
+			fmt.Printf("Error creating snapshots directory: %v\n", err)
+			return
+		}
+		fmt.Println("Created snapshots directory")
+	}
+
+	timestamp := time.Now().Format("20060102_150405")
+	snapshotFilename := fmt.Sprintf("%s/snapshot_%s.txt", snapshotsDir, timestamp)
+
+	snapshotFile, err := os.Create(snapshotFilename)
+	if err != nil {
+		fmt.Printf("Error creating snapshot file: %v\n", err)
+		return
+	}
+	defer snapshotFile.Close()
+
+	snapshotFile.WriteString(fmt.Sprintf("SNAPSHOT CREATED AT: %s\n", timestamp))
+	snapshotFile.WriteString("----------------------------------------\n")
+
+	for i := 0; i < remoteList.Count; i++ {
+		snapshotFile.WriteString(fmt.Sprintf("LIST %d (size=%d): ", i, remoteList.Lists[i].Size))
+
+		if len(remoteList.Lists[i].List) > 0 {
+			for j, value := range remoteList.Lists[i].List {
+				if j > 0 {
+					snapshotFile.WriteString(", ")
+				}
+				snapshotFile.WriteString(fmt.Sprintf("%d", value))
+			}
+		} else {
+			snapshotFile.WriteString("empty")
+		}
+
+		snapshotFile.WriteString("\n")
+	}
+
+	fmt.Printf("Snapshot created: %s\n", snapshotFilename)
 }
